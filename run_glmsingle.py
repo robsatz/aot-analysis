4021f20af925054010889cb06e9c392cc0c5ca23
robsatz <83477742+robsatz@users.noreply.github.com>
2024-05-29 16:46:05 +0200

    complete glmsingle

--- run_glmsingle.py
+++ run_glmsingle.py
@@ -12,31 +12,17 @@ def run_glmsingle(subject, session):
 
     design_matrices_aot, design_matrices_control = create_session_design_matrices(
         subject, session)
-    # bold_data_session = []
-    # for run_idx in range(len(design_matrices_aot)):
     bold_data = construct_bold_for_one_session(
         subject, session, 'T1W', 'nordicstc')
 
     output_dir = DIR_OUTPUT / \
         f'sub-{subject.zfill(3)}' / f'ses-{session}' / 'GLMsingle'
     os.makedirs(output_dir, exist_ok=True)
-    outputtype = [1, 1, 1, 1]
-    opt = dict()
-    # set important fields for completeness (but these would be enabled by default)
-    opt["wantlibrary"] = 1
-    opt["wantglmdenoise"] = 1
-    if outputtype[-1] == 1:
-        opt["wantfracridge"] = 1
-    else:
-        opt["wantfracridge"] = 0
-    # for the purpose of this example we will keep the relevant outputs in memory
-    # and also save them to the disk
-    opt["wantfileoutputs"] = outputtype
-    opt["wantmemoryoutputs"] = outputtype
-    glm = GLM_single(opt)
-    glm.fit(design=design_matrices_aot, data=bold_data, stimdur=2.5,
-            tr=0.9, outputdir=str(output_dir), figuredir=str(output_dir))
-    # TODO: repeat for control design matrix
+    for design_matrices_type in (design_matrices_aot, design_matrices_control):
+        glm = GLM_single(wantlibrary=1, wantglmdenoise=1,
+                         wantfracridge=1, wantfileoutputs=[1, 1, 1, 1])
+        glm.fit(design=design_matrices_type, data=bold_data, stimdur=2.5,
+                tr=0.9, outputdir=str(output_dir), figuredir=str(output_dir))
 
 
 DIR_DATA = Path(
