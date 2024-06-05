ae7e41d0142012ce20613d61f5fa7bb886ac35a6
robsatz <83477742+robsatz@users.noreply.github.com>
2024-06-05 18:04:28 +0200

    fix glmsingle param bug

--- run_glmsingle.py
+++ run_glmsingle.py
@@ -7,13 +7,14 @@ import argparse
 from create_design_matrices import create_session_design_matrices
 from aot.analysis.glmsingle.code_mainexp.design_constrct import construct_bold_for_one_session
 import yaml
+import copy
 
 
 def run_glmsingle(subject, session):
 
     design_aot, design_control = create_session_design_matrices(
         subject, session)
-    designs = {'aot': design_aot, 'control': design_control}
+    designs = {'control': design_control, 'aot': design_aot}
     bold_data = construct_bold_for_one_session(
         subject, session, DATATYPE, NORDICTYPE)
 
@@ -22,8 +23,8 @@ def run_glmsingle(subject, session):
         output_dir = DIR_OUTPUT / \
             f'sub-{subject.zfill(3)}' / f'ses-{session}' / f'GLMsingle_{label}'
         os.makedirs(output_dir, exist_ok=True)
-
-        glm = GLM_single(PARAMS)
+        print(PARAMS)
+        glm = GLM_single(copy.deepcopy(PARAMS))
         glm.fit(design=design, data=bold_data, stimdur=2.5,
                 tr=0.9, outputdir=str(output_dir), figuredir=str(output_dir))
 
