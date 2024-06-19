4a971217d4b62100d6a374e47cdd7a3d41460856
robsatz <83477742+robsatz@users.noreply.github.com>
2024-06-19 11:02:41 +0200

    fracridge use aot r2 for control

--- fracridge_run.py
+++ fracridge_run.py
@@ -5,14 +5,24 @@ from fracridge import FracRidgeRegressorCV
 import argparse
 import pickle
 import os
+import nibabel as nib
 
 
-def load_data(subject, session, condition):
+def load_motion_energy():
+    return np.load(DIR_MOTION_ENERGY / 'motion_energy_all.npy')
 
+
+def load_r2(subject):
+    # using subject's overall mean R2 to select voxels
+    return nib.load(DIR_DERIVATIVES /
+                    f'sub-{subject}/GLMsingle_analysis/sub-{subject}_R2_aot_mean.nii.gz').get_fdata()
+
+
+def load_amplitudes(subject, session, condition):
+    # beta weights from GLMsingle treated as neural response amplitudes
     glmsingle = np.load(DIR_DERIVATIVES /
                         f'sub-{subject}/ses-{session}/GLMsingle_{condition}/TYPED_FITHRF_GLMDENOISE_RR.npy',  allow_pickle=True).item()
-    # beta weights from GLMsingle treated as neural response amplitudes
-    return glmsingle['betasmd'], glmsingle['R2']
+    return glmsingle['betasmd']
 
 
 def select_voxels(amplitudes, r2, threshold=None):
@@ -36,8 +46,8 @@ def select_voxels(amplitudes, r2, threshold=None):
 
 
 def create_design_matrix(motion_energy, n_trials, subject, session):
-    # n_trials x n_channels
-    design_matrix = np.zeros((n_trials, motion_energy.shape[1]))
+    n_channels = motion_energy.shape[1]
+    design_matrix = np.zeros((n_trials, n_channels))
     session_trial_sequence = []
     for run_idx in range(10):
         with open(DIR_SETTINGS / f'experiment_settings_sub_{subject[1:]}_ses_{session}_run_{str(run_idx + 1).zfill(2)}.yml') as f:
@@ -47,8 +57,13 @@ def create_design_matrix(motion_energy, n_trials, subject, session):
             label for label in run_trial_sequence if label != 'blank']
         session_trial_sequence.extend(run_trial_sequence)
 
+    # chronologically inserting motion energy features into dm
     for trial_idx, trial_label in enumerate(session_trial_sequence):
-        design_matrix[trial_idx, :] = motion_energy[int(trial_label[:4])-1, :]
+        source_video_idx = int(trial_label[:4])-1
+        design_matrix[trial_idx, :] = motion_energy[source_video_idx, :]
+
+    np.save(DIR_DERIVATIVES /
+            f'sub-{subject}_ses-{session}_design_matrix_fracridge.npy', design_matrix)
     return design_matrix
 
 
@@ -59,14 +74,14 @@ def z_score(array, axis=0):
 
 
 def model(X, y, cv):
-    # specifying default parameters for clarity
     model = FracRidgeRegressorCV(normalize=False, cv=cv)
-    model.fit(X, y)
+    fracgrid = np.linspace(0.1, 1, 10)
+    model.fit(X, y, frac_grid=fracgrid)
     return model
 
 
 def save_model(model, subject, session, condition):
-    path = DIR_DERIVATIVES / \
+    path = DIR_DERIVATIVES /\
         f'sub-{subject}/ses-{session}/fracridge_{condition}'
     os.makedirs(path, exist_ok=True)
     with open(path / 'model.pkl', 'wb') as f:
@@ -75,11 +90,14 @@ def save_model(model, subject, session, condition):
     np.save(path / 'alphas.npy', model.alpha_)
 
 
-def main(subject, session, threshold, cv):
-    motion_energy = np.load(DIR_MOTION_ENERGY / 'motion_energy_all.npy')
+def run_fracridge(subject, session, threshold, cv):
+    motion_energy = load_motion_energy()
+    r2 = load_r2(subject)
 
     for condition in ('aot', 'control'):
-        amplitudes, r2 = load_data(subject, session, condition)
+        amplitudes = load_amplitudes(subject, session, condition)
+        print(
+            f'Running FracRidge for subject {int(subject)}, session {session}, condition {condition}')
         print(f'Shapes before transformations - motion energy: {motion_energy.shape} and amplitudes: {amplitudes.shape}'
               )
 
@@ -107,12 +125,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-sub", "--subject", type=int, default=1,
                         help="Subject number.")
-    parser.add_argument("-ses", "--session", type=int, default=1,
-                        help="Session number.")
     args = parser.parse_args()
     subject = str(args.subject).zfill(3)
-    session = str(args.session).zfill(2)
+
     threshold = core_settings['fracridge']['threshold']
     cv = core_settings['fracridge']['cv']
 
-    main(subject, session, threshold, cv)
+    for session in range(1, 6):
+        session = str(session).zfill(2)
+        run_fracridge(subject, session, threshold, cv)
