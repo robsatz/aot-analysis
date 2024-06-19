a967a5260c75e0688179422a77df983eac914462
robsatz <83477742+robsatz@users.noreply.github.com>
2024-06-19 14:51:04 +0200

    glmsingle save design matrix labels

--- glmsingle_create_design_matrix.py
+++ glmsingle_create_design_matrix.py
@@ -28,7 +28,7 @@ def get_tr_sequence(subject, session, run, shift=0):
     return tr_sequence
 
 
-def get_feature_indices(tr_label, prior_videos):
+def label2idx(tr_label, prior_videos):
     """
     Given a TR label, assigns an index for the design matrix according to video direction .
     Assumes TR labels of the format '0001_rv.mp4'.
@@ -46,6 +46,23 @@ def get_feature_indices(tr_label, prior_videos):
     return feature_idx_aot, feature_idx_control, prior_videos
 
 
+def idx2label(feature_idx):
+    """
+    Given a feature index, returns the corresponding label.
+    """
+    video_id = feature_idx // 2 + 1
+
+    condition = feature_idx % 2
+    aot_suffix = {0: '_fw', 1: '_rv'}
+    control_suffix = {0: '_1st', 1: '_2nd'}
+
+    label = str(video_id).zfill(4)
+    label_aot = label + aot_suffix[condition]
+    label_control = label + control_suffix[condition]
+
+    return label_aot, label_control
+
+
 def create_run_design_matrices(tr_sequence, n_features, prior_videos):
     # initialize design matrices with shape: number of TRs x number of all possible TR labels (unused columns are later pruned)
     design_matrix_aot = np.zeros(
@@ -55,7 +72,7 @@ def create_run_design_matrices(tr_sequence, n_features, prior_videos):
 
     for tr_index, tr_label in enumerate(tr_sequence):
         if tr_label != BLANK:
-            feature_idx_aot, feature_idx_control, prior_videos = get_feature_indices(
+            feature_idx_aot, feature_idx_control, prior_videos = label2idx(
                 tr_label, prior_videos)
             design_matrix_aot[tr_index][feature_idx_aot] = 1
             design_matrix_control[tr_index][feature_idx_control] = 1
@@ -85,7 +102,7 @@ def create_session_design_matrices(subject, session):
         design_matrices_aot.append(design_matrix_aot)
         design_matrices_control.append(design_matrix_control)
 
-    # remove unused features from design matrices
+   # get indices of features used in session
     used_indices_aot = used_indices_control = set()
     for run_idx in range(n_runs):
         used_indices_aot.update(
@@ -93,6 +110,23 @@ def create_session_design_matrices(subject, session):
         used_indices_control.update(
             set(np.where(np.sum(design_matrices_control[run_idx], axis=0) != 0)[0]))
 
+    # remap indices to labels - assumes alphabetical order (i.e., '0001_fw' < '0001_rv' and '0001_1st' < '0001_2nd')
+    mapping_aot = {idx: idx2label(feat_idx)[0]
+                   for idx, feat_idx in enumerate(sorted(used_indices_aot))}
+    mapping_control = {idx: idx2label(feat_idx)[1]
+                       for idx, feat_idx in enumerate(sorted(used_indices_control))}
+
+    # save as yaml
+    subject = subject.zfill(3)
+    output_path = DIR_OUTPUT / \
+        f'sub-{subject}' / f'ses-{session}' / 'design_matrices'
+    os.makedirs(output_path, exist_ok=True)
+    with open(output_path / 'feature_mapping_aot.yml', 'w') as f:
+        yaml.dump(mapping_aot, f)
+    with open(output_path / 'feature_mapping_control.yml', 'w') as f:
+        yaml.dump(mapping_control, f)
+
+    # remove unused features from design matrices
     for run_idx in range(n_runs):
         design_matrices_aot[run_idx] = design_matrices_aot[run_idx][:, list(
             used_indices_aot)]
@@ -100,10 +134,6 @@ def create_session_design_matrices(subject, session):
             used_indices_control)]
 
         # save design matrices
-        subject = subject.zfill(3)
-        output_path = DIR_OUTPUT / \
-            f'sub-{subject}' / f'ses-{session}' / 'design_matrices'
-        os.makedirs(output_path, exist_ok=True)
         np.save(output_path /
                 f'sub-{subject}_ses-{session}_run-{str(run_idx+1).zfill(2)}_design_matrix_aot.npy', design_matrices_aot[run_idx])
         np.save(output_path /
